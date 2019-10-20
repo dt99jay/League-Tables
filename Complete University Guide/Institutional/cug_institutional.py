d59cd1686e40a9e336841a1c433c2fe289a05b1a
Jamie Young <dt99jay@gmail.com>
2019-10-20 09:02:43 +0100

    CUG Institutional first commit

---
+++ Complete University Guide/Institutional/cug_institutional.py
@@ -0,0 +1,74 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def get_cols(table):
+    header = table.find_all("th")
+    cols = []
+    for column in header:
+        try:
+            col = column.find("a").get_text()
+        except AttributeError:
+            col = column.get_text()
+        cols.append(col.strip())
+        if column.get("colspan"):
+            for c in range(1, int(column.get("colspan"))):
+                cols.append("{} {}".format(col.strip(), c))
+    return cols
+
+def get_data(years):
+    url = "https://www.thecompleteuniversityguide.co.uk/league-tables/rankings?v=wide&y="
+    data = []
+    for year in years:
+        r = requests.get(url + str(year))
+        soup = BeautifulSoup(r.text, "lxml")
+        table = soup.find("table", {"class": "league-table-table"})
+        table_cols = get_cols(table)
+        table_data = []
+        for row in table.find_all("tr"):
+            table_row = []
+            for cell in row.find_all("td"):
+                table_row.append(cell.get_text().strip())
+            table_data.append(table_row)
+        table_data = [l for l in table_data if len(l) > 2]
+        table_data = pd.DataFrame(data=table_data, columns=table_cols)
+        table_data = pd.melt(table_data, id_vars=["University Name"], var_name="Metric", value_name="Value")
+        table_data["Year"] = year
+        data.append(table_data)
+    return pd.concat(data, axis=0)
+
+def clean_data(data):
+    """
+    Remove unnecessary metrics, add numerical values
+    """
+    data.rename(columns={"University Name": "Institution"}, inplace=True)
+    metrics = ["Entry Standards", "Student Satisfaction", "Research Quality", "Graduate Prospects", "Student-Staff Ratio", "Academic Services Spend", "Facilities Spend", "Good Honours", "Degree Completion", "Overall Score", "Research Intensity"]
+    data = data.copy().loc[data["Metric"].isin(metrics)]
+    data["Numeric Value"] = data["Value"].astype(str)
+    data["Numeric Value"] = data["Numeric Value"].replace("n/a", "", regex=False)
+    data["Numeric Value"] = data["Numeric Value"].str.replace(",", "", regex=False)
+    data["Numeric Value"] = data["Numeric Value"].str.replace(r"[a-z]", "", regex=True)
+    data["Numeric Value"] = pd.to_numeric(data["Numeric Value"], errors="coerce") # Coerce will turn blanks to NaNs
+    return data
+
+def rank_metrics(data):
+    """
+    Calculate rank and decile for each metric by year
+    """
+    data_exc_ssr = data.copy().loc[data["Metric"] != "Student-Staff Ratio"] # All metrics except SSR are ranked descending
+    data_exc_ssr["Rank"] = data_exc_ssr.groupby(["Year", "Metric"])["Numeric Value"].rank(ascending=False, method="min")
+    data_exc_ssr["Decile"] = data_exc_ssr.groupby(["Year", "Metric"])["Numeric Value"].transform(
+        lambda x: pd.qcut(x.rank(method="first"), 10, labels=range(1,11)) # Calculate deciles on ranked data to avoid duplicate bin edges as https://stackoverflow.com/a/40548606/2950747
+    )
+    data_inc_ssr = data.copy().loc[data["Metric"] == "Student-Staff Ratio"] # SSR is ranked ascending
+    data_inc_ssr["Rank"] = data_inc_ssr.groupby(["Year", "Metric"])["Numeric Value"].rank(ascending=True, method="min")
+    data_inc_ssr["Decile"] = data_inc_ssr.groupby(["Year", "Metric"])["Numeric Value"].transform(
+        lambda x: pd.qcut(x.rank(method="first"), 10, labels=list(reversed(range(1,11)))) # Calculate deciles on ranked data to avoid duplicate bin edges as https://stackoverflow.com/a/40548606/2950747
+    )
+    return data_exc_ssr.append(data_inc_ssr)
+
+years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
+data = get_data(years)
+data = clean_data(data)
+data = rank_metrics(data)
+data.to_csv("Complete University Guide Institutional.csv", index=False) # Save final CSV to disk
